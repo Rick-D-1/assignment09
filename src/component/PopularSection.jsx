0d1bbd233c53e60e4442f45d2c49a913fbda9fcc
Rick <rickdev564@gmail.com>
2025-11-25 02:03:38 +0600

    2nd commit

--- src/component/PopularSection.jsx
+++ src/component/PopularSection.jsx
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const PopularSection = () => {
+
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch('/public/sevices.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+            .catch(err => console.log(err))
+
+    }, [])
+    console.log(services);
+
+
+
     return (
-        <div className='mt-8'>
-            <h3 className='text-3xl font-bold text-center'>Popular Game Section</h3>
+        <div className='mt-8 px-[145px]'>
+            <div>
+                <h3 className='text-3xl font-bold text-center'>Popular Game Section</h3>
+            </div>
+            <div className=' grid grid-cols-1 lg:grid-cols-3  mt-7 mb-6'>
+                {
+                    services.map(service => <div className="card bg-base-100 w-96 shadow-sm mt-8 rounded-lg">
+                        <figure>
+                            <img className='w-full h-[300px] object-cover'
+                                src={service.coverPhoto
+                                }
+                                alt="Shoes" />
+                        </figure>
+                        <div className="card-body shadow-2xl ">
+                            <h2 className="card-title">{service?.title}</h2>
+                            <p>{service.description}</p>
+                            <div className='flex justify-between'>
+                                <p className='font-bold'>Developer : <span className='text-green-600'>{service.developer}</span></p>
+                                <p className='font-bold'> Ratings : <span className='text-yellow-600 '>{service.ratings}</span></p>
+                            </div>
+                            <div className=' mt-3 text-center h-7 text-lg font-bold rounded-lg text-white bg-blue-500'>
+                                <button >View Details</button>
+                            </div>
+                        </div>
+                    </div>)
+                }
+
+
+            </div>
+
         </div>
+
     );
 };
 
