a56545e558dc6a77766bdeb635c3547191180552
Rick <rickdev564@gmail.com>
2025-12-09 23:12:49 +0600

    first commit

--- src/component/PopularSection.jsx
+++ src/component/PopularSection.jsx
@@ -18,23 +18,23 @@ const PopularSection = () => {
     return (
         <div className='mt-8 px-[145px]'>
             <div>
-                <h3 className='text-3xl font-bold text-center'>Popular <span className='text-orange-500'>Game</span> Section</h3>
+                <h3 className='text-3xl font-bold text-center'>Popular <span className='text-orange-500'> Section</span></h3>
             </div>
             <div className=' grid grid-cols-1 lg:grid-cols-3  mt-7 mb-6'>
                 {
                     services.slice(0, 6).map(service => <div className="card bg-base-100 w-96 shadow-sm mt-8 rounded-lg">
                         <figure>
                             <img className='w-full h-[300px] object-cover'
-                                src={service.coverPhoto
+                                src={service.image
                                 }
                                 alt="Shoes" />
                         </figure>
                         <div className="card-body shadow-2xl ">
-                            <h2 className="card-title">{service?.title}</h2>
+                            <h2 className="card-title">{service?.serviceName}</h2>
                             <p>{service.description}</p>
                             <div className='flex justify-between'>
-                                <p className='font-bold'>Developer : <span className='text-green-600'>{service.developer}</span></p>
-                                <p className='font-bold'> Ratings : <span className='text-yellow-600 '>{service.ratings}</span></p>
+                                <p className='font-bold'>Price : <span className='text-green-600'>{service.price}$</span></p>
+                                <p className='font-bold'> Ratings : <span className='text-yellow-600 '>{service.rating}</span></p>
                             </div>
 
                         </div>
